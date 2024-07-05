52977b835ff1da7f42e93eaeb106663f42f9b01a
Fahmidur Rahaman Shanto <fahmidurrahamanshanto@gmail.com>
2024-07-06 01:27:13 +0600

    created initial car doctors backend server with mongodb connection

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clustercrud.ctitxen.mongodb.net/?retryWrites=true&w=majority&appName=clusterCrud`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    app.get("/", (req, res) => {
+      res.send("Welcome to Cars Doctors Server");
+    });
+
+    app.listen(port, (req, res) => {
+      console.log(`Cars Doctors server is running on port: ${port}`);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
